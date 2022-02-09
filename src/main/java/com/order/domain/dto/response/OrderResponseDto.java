7b41391b741884f73fc5993b174e29c0067744bc
Marcos Barbosa <marcos_pp.16@hotmail.com>
2022-02-09 16:16:59 -0300

    Implementando a anotação @ApiModelProperty na camada de dto

--- src/main/java/com/order/domain/dto/response/OrderResponseDto.java
+++ src/main/java/com/order/domain/dto/response/OrderResponseDto.java
@@ -9,17 +9,24 @@ import com.order.domain.model.Customer;
 import com.order.domain.model.DeliveryAddress;
 import com.order.domain.model.Items;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class OrderResponseDto {
+	@ApiModelProperty(value = "id do pedido", example = "123")
 	private Long id;
-	private Long idLong;
+	@ApiModelProperty(value = "id da oferta", example = "1234")
 	private Long idOffer;
+	@ApiModelProperty(value = "data do instante do pedido", example = "dd/MM/yyyy")
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate instante;
+	@ApiModelProperty(value = "valor total do pedido", example = "240")
 	private BigDecimal valorTotal;
+	@ApiModelProperty(value = "nome do consumidor", example = "Maria")
 	private Customer consumer;
+	@ApiModelProperty(value = "Endereço de entrega", example = "Rua 4...")
 	private DeliveryAddress deliveryAddress;
+	@ApiModelProperty(value = "Itens do pedido", example = "Dorflex, tossilax")
 	private List<Items> items;
 }
